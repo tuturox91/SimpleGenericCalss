491569f4aa9afd67419b3d74b6bc85299ce831b2
John Doe <tuturox91@gmail.com>
2022-10-06 13:08:17 +0300

    Simple Generic class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        var result = someWork(10, 30);
+        System.out.println(result.getFirst());
+        System.out.println(result.getSecond());
+    }
+
+    public  static Pair someWork(int firstNumber, int secondNumber) {
+        firstNumber += (10 * 2)-firstNumber; //some work
+        secondNumber += (10 * 3)+firstNumber;
+
+        return new Pair(firstNumber, secondNumber);
     }
 }
